23be163b977f5e501f82fda0c31dc254770de4c3
Ranjgith <84561561+ranjgith-s@users.noreply.github.com>
2025-08-11 22:32:05 +0530

    keep ui-bridge

---
+++ src/components/ui-bridge/index.ts
@@ -0,0 +1,53 @@
+// DEPRECATED bridge layer. Migrate imports to '../ui'. Remaining exports kept for legacy props.
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  CardBody,
+  CardDescription,
+  CardTitle,
+} from './card';
+export { Button } from './button';
+export { Badge } from './badge';
+export { Avatar } from './avatar';
+export { Spinner } from '../ui/spinner';
+export * from '../ui/button';
+export * from '../ui/badge';
+export * from '../ui/avatar';
+export * from '../ui/input';
+export * from '../ui/textarea';
+export * from '../ui/switch';
+export * from '../ui/spinner';
+export { Breadcrumbs, BreadcrumbItem } from './breadcrumbs';
+export { Divider } from './divider';
+export { Link } from './link';
+export { Kbd } from './kbd';
+export { ScrollShadow } from './scroll-shadow';
+export {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from './modal';
+export { Snippet } from './snippet';
+export { ButtonGroup } from './button-group';
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from './table';
+export {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from './dropdown';
+export { Pagination } from './pagination';
+export { Select, SelectItem } from './select';
+export { Chip } from './chip';
+export { Autocomplete, AutocompleteItem } from './autocomplete';
